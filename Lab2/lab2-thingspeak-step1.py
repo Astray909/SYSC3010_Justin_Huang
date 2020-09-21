bfc081bd5470c51a44067cf798662ef57eb87fd0
Justin Huang <justin.huang3@cmail.ca>
2020-09-21 19:00:46 -0400

    part 2 1

---
+++ Lab2/lab2-thingspeak-step1.py
@@ -0,0 +1,34 @@
+import httplib
+import urllib
+import time
+key = "VVMPLOTIBDHL0DCX"  # Put your API Key here
+def thermometer():
+    while True:
+        #Calculate CPU temperature of Raspberry Pi in Degrees C
+        temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
+        params = urllib.urlencode({'field1': temp, 'key':key }) 
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print temp
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+        except:
+            print "connection failed"
+        break
+if __name__ == "__main__":
+        while True:
+                thermometer()
+
+while True:
+    events = sense.stick.get_events()
+    if events:
+        for event in events:
+            if event.action != 'pressed':
+                continue
+            elif event.direction == 'middle':
+                sense.set_pixels(NONE)
+                exit()
