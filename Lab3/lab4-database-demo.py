be45b7031d94e3fc1786e3a7a3cf91f419371013
Justin Huang <justin.huang3@cmail.ca>
2020-09-28 14:35:03 -0400

    lab 3 done

---
+++ Lab3/lab4-database-demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sqlite3
+#connect to database file
+dbconnect = sqlite3.connect("demo.db");
+dbconnect.row_factory = sqlite3.Row;
+cursor = dbconnect.cursor();
+
+cursor.execute('''CREATE TABLE sensor (sensorID NUMERIC, type TEXT, zone TEXT);''');
+cursor.execute('''INSERT INTO sensor values(1, "door", "kitchen")''');
+cursor.execute('''INSERT INTO sensor values(2, "temperature", "kitchen")''');
+cursor.execute('''INSERT INTO sensor values(3, "door", "garage")''');
+cursor.execute('''INSERT INTO sensor values(4, "motion", "garage")''');
+cursor.execute('''INSERT INTO sensor values(5, "temperature", "garage")''');
+
+cursor.execute('SELECT * FROM sensor WHERE zone="garage"');
+for row in cursor:
+    print(row['sensorID'],row['type']);
+
+cursor.execute('SELECT * FROM sensor WHERE zone="kitchen"');
+for row in cursor:
+    print(row['sensorID'],row['type']);
+
+dbconnect.close();
