7f44a65f1b2db5555ce40f8641c7b95171092d92
Justin Huang <justin.huang3@cmail.ca>
2020-09-28 19:17:46 -0400

    Part 1 complete

--- Lab3/lab4-database-demo.py
+++ Lab3/lab4-database-demo.py
@@ -6,11 +6,14 @@ dbconnect.row_factory = sqlite3.Row;
 cursor = dbconnect.cursor();
 
 cursor.execute('''CREATE TABLE sensor (sensorID NUMERIC, type TEXT, zone TEXT);''');
+
 cursor.execute('''INSERT INTO sensor values(1, "door", "kitchen")''');
 cursor.execute('''INSERT INTO sensor values(2, "temperature", "kitchen")''');
 cursor.execute('''INSERT INTO sensor values(3, "door", "garage")''');
 cursor.execute('''INSERT INTO sensor values(4, "motion", "garage")''');
 cursor.execute('''INSERT INTO sensor values(5, "temperature", "garage")''');
+
+dbconnect.commit();
 
 cursor.execute('SELECT * FROM sensor WHERE zone="garage"');
 for row in cursor:
